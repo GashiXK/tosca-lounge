c92fef2e919a9c9bb88ab2856aeae039a09785d0
GashiXK <gashi449116@gmail.com>
2025-06-20 09:55:04 +0200

    Update menuSection.vue

--- components/menuSection.vue
+++ components/menuSection.vue
@@ -41,7 +41,7 @@
           name: "Supë pule",
           description: "Supë e ngrohtë me copëza pule",
           price: 2.00,
-          isVegetarian: true
+          isVegetarian: false
         },
         {
           id: 2,
@@ -55,14 +55,14 @@
 
     {
       id: 2,
-      name: 'Mëngjesi',
+      name: 'Mëngjes',
       description: 'Filloni ditën tuaj me energji',
       items: [
         {
           id: 3,
-          name: "Mëngjesi Tosca",
-          description: "",
-          price: 2.50,
+          name: "Mëngjes Tosca",
+          description: "Kaqkavall,Perime, Proshutë, Sallatë, Suxhukë",
+          price: 3.00,
           isVegetarian: false
         },
         {
@@ -81,7 +81,7 @@
         },
         {
           id: 6,
-          name: "Llokuma me Djath",
+          name: "Llokuma me Djathë",
           description: "",
           price: 2.00,
           isVegetarian: false
@@ -89,51 +89,468 @@
         {
           id: 7,
           name: "Omlet Proshutë",
-          description: "Sallatë me djathë dhe hajvar",
+          description: "Sallatë, Djathë, Hajvar",
           price: 2.00,
           isVegetarian: false
         },
         {
           id: 8,
           name: "Omlet Djathë",
-          description: "Sallatë me djathë dhe hajvar",
-          price: 2.00,
-          isVegetarian: false
-        },
-        {
-          id: 9,
-          name: "Omlet Kërpudha",
-          description: "Sallatë me djathë dhe hajvar",
+          description: "Sallatë, Djathë, Hajvar",
           price: 2.00,
           isVegetarian: false
         },
       ]
     },
-    
-
 
     {
       id: 3,
-      name: 'Chef Specials',
-      description: 'Exclusive creations from our master chef',
-      gridColumns: 'grid-cols-1',
+      name: 'Senduiç',
+      description: 'Shije të freskëta dhe të shijshme në çdo kafshatë',
       items: [
         {
-          id: 5,
-          name: "Chef's Tasting Menu",
-          description: "Five-course seasonal tasting experience",
-          price: 75,
-          isChefSpecial: true
+          id: 9,
+          name: "Senduiç Pule",
+          description: "Djathë, Sallatë, Pomfrit, Sos",
+          price: 2.00,
+          isVegetarian: false
         },
         {
-          id: 6,
-          name: "Vegetarian Degustation",
-          description: "Five-course plant-based tasting menu",
-          price: 65,
-          isChefSpecial: true
-        }
+          id: 10,
+          name: "Senduiç Proshuto",
+          description: "Djathë, Sallatë, Pomfrit, Sos",
+          price: 2.00,
+          isVegetarian: false
+        },
+        {
+          id: 11,
+          name: "Senduiç Peperoni",
+          description: "Sallatë, Pomfrit, Sos",
+          price: 2.00,
+          isVegetarian: false
+        },
+        {
+          id: 12,
+          name: "Senduiç Tosca",
+          description: "Pomfrit, Sallatë, Proshutë, Sos",
+          price: 3.00,
+          isVegetarian: false
+        },
+        {
+          id: 13,
+          name: "Senduiç Tosca Pule",
+          description: "Pomfrit, Sallatë, Sos",
+          price: 3.00,
+          isVegetarian: false
+        },
       ]
+    },
+
+    {
+      id: 4,
+      name: 'Pasta',
+      description: 'Përvoja e shijes italiane në çdo kafshatë',
+      items: [
+        {
+          id: 14,
+          name: "Pasta Aglio Olio",
+          description: "Hudhër, Djeks, Verë e bardhë - sipas dëshirës",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 15,
+          name: "Pasta Arabiata",
+          description: "Qepë, domate, fafarona",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 16,
+          name: "Pasta Carbonara",
+          description: "Proshutë, vezë",
+          price: 3.00,
+          isVegetarian: false
+        },
+        {
+          id: 17,
+          name: "Pasta Boloneze",
+          description: "Qepë, mish i bluar",
+          price: 3.00,
+          isVegetarian: false
+        },
+      ]
+    },
+
+    {
+      id: 5,
+      name: 'Paragjella',
+      description: 'Shije të freskëta dhe të shijshme në çdo kafshatë',
+      items: [
+        {
+          id: 18,
+          name: "Pomfrit",
+          description: "Djathë",
+          price: 2.00,
+          isVegetarian: false
+        },
+        {
+          id: 19,
+          name: "Paragjellë Tosca",
+          description: "Proshutë, Suxhukë, Djathë",
+          price: 3.50,
+          isVegetarian: false
+        },
+        {
+          id: 20,
+          name: "Kërpudha të fërguara",
+          description: "Qepë",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 21,
+          name: "Chicken Fingers",
+          description: "Sos, Pomfrit",
+          price: 4.00,
+          isVegetarian: false
+        },
+        {
+          id: 22,
+          name: "Djath Porcion",
+          description: "",
+          price: 2.50,
+          isVegetarian: false
+        },
+      ]
+    },
+    
+    {
+      id: 6,
+      name: 'Hamburger',
+      description: 'Hamburger të shijshëm dhe të freskët',
+      items: [
+        {
+          id: 23,
+          name: "Hamburger",
+          description: "Pomfrit, Sallatë, Sos",
+          price: 1.80,
+          isVegetarian: false
+        },
+        {
+          id: 24,
+          name: "Hamburger me Veze",
+          description: "Pomfrit, Sallatë, Sos",
+          price: 2.00,
+          isVegetarian: false
+        },
+        {
+          id: 25,
+          name: "Cheese Burger",
+          description: "Pomfrit, Djathë, Sallatë, Sos",
+          price: 2.00,
+          isVegetarian: false
+        },
+        {
+          id: 26,
+          name: "Chicken Burger",
+          description: "Pomfrit, Mish Pule, Sallatë, Sos",
+          price: 4.00,
+          isVegetarian: false
+        },
+        {
+          id: 27,
+          name: "Big Burger",
+          description: "2x Pleskavica, Pomfrit, Sallatë, Sos",
+          price: 2.50,
+          isVegetarian: false
+        },
+      ]
+    },
+    
+    {
+      id: 7,
+      name: 'Risoto',
+      description: 'Risoto të shijshëm dhe të freskët',
+      items: [
+        {
+          id: 28,
+          name: "Risoto Pule",
+          description: "Sallatë, Pomfrit",
+          price: 3.50,
+          isVegetarian: false
+        },
+        {
+          id: 29,
+          name: "Risoto Kërpudha",
+          description: "Sallatë, Pomfrit ",
+          price: 3.50,
+          isVegetarian: false
+        },
+        {
+          id: 30,
+          name: "Risoto Perime",
+          description: "Sallatë, Pomfrit ",
+          price: 3.50,
+          isVegetarian: false
+        },
+      ]
+    },
+
+    {
+      id: 8,
+      name: 'Sallata',
+      description: 'Sallata të shijshme dhe të freskëta',
+      items: [
+        {
+          id: 31,
+          name: "Sallatë Shope",
+          description: "Domate, Tranguj, Qepë, Djathë",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 32,
+          name: "Sallatë Mix",
+          description: "Sallatë gjelbërt, Domate, Tranguj, Laker, Qepë, Karrota",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 33,
+          name: "Sallatë e Gjelbërt",
+          description: "Tranguj, Qepë",
+          price: 2.50,
+          isVegetarian: false
+        },
+
+        {
+          id: 34,
+          name: "Sallatë Greke",
+          description: "Domate, Tranguj, Qepë, Djathë",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 35,
+          name: "Sallatë Tuna",
+          description: "Tuna, Tranguj, Qepë",
+          price: 3.50,
+          isVegetarian: false
+        },
+        {
+          id: 36,
+          name: "Sallatë Pule",
+          description: "Sallat e gjelbërt, Domate, Tranguj, Qepë",
+          price: 3.50,
+          isVegetarian: false
+        },
+      ]
+    },
+
+    {
+      id: 9,
+      name: 'Skara',
+      description: 'Mishra nga Skara të shijshme dhe të freskëta',
+      items: [
+        {
+          id: 37,
+          name: "Qofte me kajmak",
+          description: "Sallatë, Pomfrit",
+          price: 3.50,
+          isVegetarian: false
+        },
+        {
+          id: 38,
+          name: "Pleskavicë ",
+          description: "Sallatë, Pomfrit",
+          price: 4.00,
+          isVegetarian: false
+        },
+        {
+          id: 39,
+          name: "Pleskavicë me sos",
+          description: "Sallatë, Pomfrit",
+          price: 4.50,
+          isVegetarian: false
+        },
+
+        {
+          id: 40,
+          name: "Pleskavicë e mbushur",
+          description: "Sallatë, Pomfrit",
+          price: 4.00,
+          isVegetarian: false
+        },
+        {
+          id: 41,
+          name: "Pleskavicë e mbushur me sos",
+          description: "Sallatë, Pomfrit",
+          price: 4.50,
+          isVegetarian: false
+        },
+        {
+          id: 42,
+          name: "File Pule në grill",
+          description: "Sallatë, Pomfrit",
+          price: 4.00,
+          isVegetarian: false
+        },
+        {
+          id: 43,
+          name: "File Pule me sos",
+          description: "Sallatë, Pomfrit, Oriz",
+          price: 4.00,
+          isVegetarian: false
+        },
+        {
+          id: 44,
+          name: "Shnicell Pule",
+          description: "Mish i bardhë i fërguar, Sallatë, Pomfrit, Sos",
+          price: 4.50,
+          isVegetarian: false
+        },
+
+        {
+          id: 45,
+          name: "Ramstek",
+          description: "Sallatë, Pomfrit, Kërpudha",
+          price: 7.00,
+          isVegetarian: false
+        },
+        {
+          id: 46,
+          name: "Kombinim mishi",
+          description: "Mish i bardhë, Ramstek, Suxhuk, Qofte, Sallatë",
+          price: 8.00,
+          isVegetarian: false
+        },
+      ]
+    },
+
+
+    {
+      id: 10,
+      name: 'Samun',
+      description: 'Samuna të shijshme',
+      items: [
+        {
+          id: 47,
+          name: " Samun Proshutë",
+          description: "Pomfrit, Sos",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 48,
+          name: "Samun Suxhuk",
+          description: "Pomfrit, Sos",
+          price: 2.50,
+          isVegetarian: false
+        },
+        {
+          id: 49,
+          name: "Samun Mix",
+          description: "Pomfrit, Sos",
+          price: 2.50,
+          isVegetarian: false
+        },
+      ]
+    },
+
+  {
+  id: 11,
+  name: 'Pizza',
+  description: 'Pizza të freskëta dhe të shijshme në çdo përmasë',
+  items: [
+    {
+      id: 50,
+      name: "Pizza Margarita",
+      description: "Sos domate, Djathë",
+      prices: {
+        small: 2.00,
+        medium: 3.00,
+        large: 4.00
+      },
+      isVegetarian: true
+    },
+    {
+      id: 51,
+      name: "Pizza Proshuto",
+      description: "Sos domatesh, Djathë, Proshutë",
+      prices: {
+        small: 2.00,
+        medium: 3.00,
+        large: 4.00
+      },
+      isVegetarian: false
+    },
+    {
+      id: 52,
+      name: "Pizza Regjina",
+      description: "Sos domatesh, Djathë, Proshutë, Kërpudha",
+      prices: {
+        small: 2.00,
+        medium: 3.00,
+        large: 4.00
+      },
+      isVegetarian: true
+    },
+    {
+      id: 53,
+      name: "Pizza Tuna",
+      description: "Sos domatesh, Djathë, Peshk, Qepë",
+      prices: {
+        small: 2.00,
+        medium: 4.00,
+        large: 5.50
+      },
+      isVegetarian: false
+    },
+    {
+      id: 54,
+      name: "Pizza Tosca",
+      description: "Sos domatesh, Kaqkavall, Proshutë, Kërpudha, Suxhuk, Krem",
+      prices: {
+        small: 2.50,
+        medium: 4.00,
+        large: 5.50
+      },
+      isVegetarian: false
+    },
+        {
+      id: 55,
+      name: "Pizza Peperoni",
+      description: "Sos domatesh, Djathë, Suxhuk",
+      prices: {
+        small: 2.50,
+        medium: 4.00,
+        large: 5.50
+      },
+      isVegetarian: false
+    },
+  ],
+  
+
+
+},
+
+
+{
+  id: 56,
+  name: 'Extra',
+  items: [
+    {
+      id: 50,
+      name: "Sos",
+      description: "CDO POROSI EXTRA PAGUHET!",
+      price: 0.50
     }
+  ]
+}
+
+
+    
   ])
   </script>
   
